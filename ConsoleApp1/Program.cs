dfa1ac3cbf1fa2244c7afd95be5e02228ca0d574
janthony25 <junanthony.business@gmail.com>
2024-01-31 20:36:52 +1300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PyramidPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pyramid Pattern\n\n");
+
+            int row = 5;
+            int j, k;
+
+            for (int i = 0; i <= row; i++)
+            {
+
+                for(j = 1; j <= row -  i; j++)
+                    Console.Write(" ");
+
+                for (k = 1; k <= 2 * i - 1 ; k++)
+                    Console.Write("*");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
